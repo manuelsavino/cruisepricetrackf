aaf62a2b22040d7b05bf2a2e8b3589f9746fb56f
manuelsavino <manuelsavino@gmail.com>
2020-06-09 23:10:57 -0400

    lambda fetch request

---
+++ functions/cruiseSearch.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+
+const API_ENDPOINT =
+  "https://www.carnival.com/CruiseSearch/api/search?numAdults=2&pageNumber=1&pageSize=8&pastGuest=true&showBest=true&sort=fromprice&useSuggestions=true";
+
+exports.handler = async (event, context) => {
+  try {
+    response = await fetch(API_ENDPOINT);
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response,
+    }),
+  };
+};
