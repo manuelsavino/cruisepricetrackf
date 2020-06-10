7bf1e73c42529152076281c705aeb036e6125d04
manuelsavino <manuelsavino@gmail.com>
2020-06-09 23:34:53 -0400

    👀

--- functions/cruiseSearch.js
+++ functions/cruiseSearch.js
@@ -1,11 +1,10 @@
-const fetch = require("node-fetch");
-
+const axios = require("axios");
 const API_ENDPOINT =
   "https://www.carnival.com/CruiseSearch/api/search?numAdults=2&pageNumber=1&pageSize=8&pastGuest=true&showBest=true&sort=fromprice&useSuggestions=true";
 
 exports.handler = async (event, context) => {
   try {
-    response = await fetch(API_ENDPOINT);
+    response = await axios.get(API_ENDPOINT);
   } catch (err) {
     console.log(err);
     return {
